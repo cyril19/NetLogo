d536d35daef8cd6b020363d478ae603ceb578a7d
Seth Tisue <seth@tisue.net>
2012-07-24 22:10:12 -0400

    make ShapeList know whether it's turtles or links
    to permit serialization/deserialization in mirroring code

--- src/main/org/nlogo/agent/World.java
+++ src/main/org/nlogo/agent/World.java
@@ -84,8 +84,8 @@ public strictfp class World
   public volatile boolean comeUpForAir = false;  // NOPMD pmd doesn't like 'volatile'
 
   public World() {
-    _turtleShapeList = new ShapeList();
-    _linkShapeList = new ShapeList();
+    _turtleShapeList = new ShapeList(AgentKindJ.Turtle());
+    _linkShapeList = new ShapeList(AgentKindJ.Link());
 
     _observer = createObserver();
     _observers = new ArrayAgentSet(AgentKindJ.Observer(), 1, "observers", false, this);
